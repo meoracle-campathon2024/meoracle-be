a908b25302f6089d2e4ddc1191303bd913adfcb4
Vu Tung Lam <vutunglaminfo@gmail.com>
2024-08-06 16:41:39 +0700

    fix: increase character limit for column storing user image URL

---
+++ prisma/migrations/20240806091608_increase_image_file_path_limit/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `user_images` MODIFY `file_path` LONGTEXT NOT NULL;
--- src/controllers/image_model/query/helpers/data.ts
+++ src/controllers/image_model/query/helpers/data.ts
@@ -5,18 +5,27 @@ export async function saveImageModelQueryInfo({ prisma, userId, userImageFilePat
     prisma: PrismaClient,
     userId: number,
     userImageFilePaths: string[],
-}): Promise<[QueryDetail]> {
+}): Promise<[QueryDetail, ImageQuery]> {
     const queryDetail = await prisma.queryDetail.create({
         data: {
             created_at: new Date(),
             type: IMAGE_MODEL_ID,
-            image_query: {
-                create: {
-                    user_id: userId,
+        },
+    });
+
+    const imageQuery = await prisma.imageQuery.create({
+        data: {
+            user_id: userId,
+            query_detail_id: queryDetail.id,
+            user_images: {
+                createMany: {
+                    data: userImageFilePaths.map(file_path => {
+                        return { file_path };
+                    }),
                 },
             },
         },
     });
 
-    return [queryDetail];
+    return [queryDetail, imageQuery];
 }
